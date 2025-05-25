a21205c7229589792630501c417ef56ce86bd480
barkazz <72384206+barkazz@users.noreply.github.com>
2025-05-25 17:18:09 +0000

    commit

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -37,7 +37,7 @@ def run_train(data_path: str):
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
 
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = mean_squared_error(y_val, y_pred)
 
 
 if __name__ == '__main__':
