6cf923d7b8435fe39ff30e620d08106176f08e42
barkazz <72384206+barkazz@users.noreply.github.com>
2025-05-25 20:26:11 +0000

    cm

--- 02-experiment-tracking/homework/hpo.py
+++ 02-experiment-tracking/homework/hpo.py
@@ -8,7 +8,7 @@ from hyperopt.pyll import scope
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
-mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("random-forest-hyperopt")
 
 
@@ -40,10 +40,10 @@ def run_optimization(data_path: str, num_trials: int):
             rf = RandomForestRegressor(**params)
             rf.fit(X_train, y_train)
             y_pred = rf.predict(X_val)
-            rmse = mean_squared_error(y_val, y_pred)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
             mlflow.log_metric("rmse", rmse)
 
-            return {'loss': rmse, 'status': STATUS_OK}
+        return {'loss': rmse, 'status': STATUS_OK}
 
     search_space = {
         'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
