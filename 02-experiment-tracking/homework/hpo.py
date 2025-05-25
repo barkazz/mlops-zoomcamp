71867eaaf0e1723ea07a04e01ae3e2f8d5d47071
barkazz <72384206+barkazz@users.noreply.github.com>
2025-05-25 18:43:36 +0000

    cm

--- 02-experiment-tracking/homework/hpo.py
+++ 02-experiment-tracking/homework/hpo.py
@@ -8,7 +8,7 @@ from hyperopt.pyll import scope
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
 mlflow.set_experiment("random-forest-hyperopt")
 
 
@@ -39,6 +39,11 @@ def run_optimization(data_path: str, num_trials: int):
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
         rmse = mean_squared_error(y_val, y_pred, squared=False)
+
+        mlflow.log_metrics({
+            'data_path': data_path,
+            'rmse': rmse
+        })
 
         return {'loss': rmse, 'status': STATUS_OK}
 
