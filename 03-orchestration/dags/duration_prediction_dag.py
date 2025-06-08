230214928c77c49f6eb4151b2effb7b4f8835cdb
barkazz <72384206+barkazz@users.noreply.github.com>
2025-06-08 23:53:27 +0000

    try

--- 03-orchestration/dags/duration_prediction_dag.py
+++ 03-orchestration/dags/duration_prediction_dag.py
@@ -43,17 +43,13 @@ with DAG(
 
         print(df.shape[0], 'rows extracted')
 
-        df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
-        df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
-
-        df = df[(df.duration >= 1) & (df.duration <= 60)]
+        df['duration'] = (df.tpep_dropoff_datetime - df.tpep_pickup_datetime).dt.total_seconds() / 60
+        df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
         print(df.shape[0], 'rows after filtering')
 
         categorical = ['PULocationID', 'DOLocationID']
         df[categorical] = df[categorical].astype(str)
-
-        df['PU_DO'] = df['PULocationID'] + '_' + df['DOLocationID']
 
         return df
 
