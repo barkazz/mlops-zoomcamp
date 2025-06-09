ad25f5d6b460b7cd0449efc655144fc250e1ccba
barkazz <72384206+barkazz@users.noreply.github.com>
2025-06-09 16:37:15 +0000

    dag check

--- 03-orchestration/homework/duration_prediction_dag copy.py
+++ 03-orchestration/homework/duration_prediction_dag copy.py
@@ -36,13 +36,15 @@ with DAG(
     default_args=default_args,
 ) as dag:
 
-    def extract_data(year, month):
-        url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02d}.parquet'
+    def extract_data():
+
+        url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet'
         df = pd.read_parquet(url)
+
         print(df.shape[0], 'rows extracted')
 
-        df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
-        df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+        df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+        df.duration = df.duration.dt.total_seconds() / 60
 
         df = df[(df.duration >= 1) & (df.duration <= 60)]
 
@@ -51,15 +53,13 @@ with DAG(
         categorical = ['PULocationID', 'DOLocationID']
         df[categorical] = df[categorical].astype(str)
 
-        df['PU_DO'] = df['PULocationID'] + '_' + df['DOLocationID']
-
         return df
 
 
     # Define tasks
     t1 = PythonOperator(
         task_id='extract_data',
-        python_callable=extract_data(2023, 3)
+        python_callable=extract_data,
     )
 
 """    t2 = PythonOperator(
@@ -78,4 +78,4 @@ with DAG(
     )"""
 
     # Task dependencies
-    t1 #>> t2 >> t3 >> t4
+t1 #>> t2 >> t3 >> t4
