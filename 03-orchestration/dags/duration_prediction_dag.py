a4e714576c1415eb5769691dbfa3ec51981ffaaa
barkazz <72384206+barkazz@users.noreply.github.com>
2025-06-09 19:34:43 +0000

    f

--- 03-orchestration/dags/duration_prediction_dag.py
+++ 03-orchestration/dags/duration_prediction_dag.py
@@ -9,7 +9,7 @@ from sklearn.metrics import mean_squared_error
 import mlflow
 import mlflow.sklearn
 
-mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_tracking_uri("http://0.0.0.0:5000")
 mlflow.set_experiment("homework-3-nyc-taxi")
 
 default_args = {
