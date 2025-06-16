8962cf124d5c794f9107bc2451f2697ffc917ae7
barkazz <72384206+barkazz@users.noreply.github.com>
2025-06-16 22:54:27 +0000

    upd py sciprt

--- 04-deployment/homework_4.py
+++ 04-deployment/homework_4.py
@@ -1,54 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import sys
+import os
 import pickle
 import pandas as pd
-import sklearn
 
 
-with open('model.bin', 'rb') as f_in:
-    dv, model = pickle.load(f_in)
+year = int(sys.argv[1]) # 2023
+month = int(sys.argv[2]) # 4
+
+input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+
+
+MODEL_FILE = os.getenv('MODEL_FILE', 'model.bin')
+
+with open(MODEL_FILE, 'rb') as f_in:
+    dv, lr = pickle.load(f_in)
+
 
 categorical = ['PULocationID', 'DOLocationID']
 
 def read_data(filename):
-    cols = ['tpep_pickup_datetime', 'tpep_dropoff_datetime', 'PULocationID', 'DOLocationID']
-    df = pd.read_parquet(filename, columns=cols)
+    df = pd.read_parquet(filename)
     
-    df['duration'] = (
-        df.tpep_dropoff_datetime
-          .sub(df.tpep_pickup_datetime)
-          .dt
-          .total_seconds()
-          .div(60)
-    )
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
 
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
     
     return df
 
 
-year = int(sys.argv[1]) #2023
-month = int(sys.argv[2]) #04
-
-df = read_data(f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet')
-output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+df = read_data(input_file)
+df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
 
 
 dicts = df[categorical].to_dict(orient='records')
 X_val = dv.transform(dicts)
-y_pred = model.predict(X_val)
+y_pred = lr.predict(X_val)
 
 
-print('predicted mean duration is', y_pred.mean())
-
-
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+print('predicted mean duration:', y_pred.mean())
 
 
 df_result = pd.DataFrame()
 df_result['ride_id'] = df['ride_id']
 df_result['predicted_duration'] = y_pred
 
+
+os.makedirs('output', exist_ok=True)
 
 df_result.to_parquet(
     output_file,
